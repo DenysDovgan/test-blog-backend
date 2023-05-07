aed7c6818ef95f7ddd0751662e55b96448dd97a6
DenysDovgan <dovgan.denys.ws@gmail.com>
2023-05-08 00:15:14 +0200

    post and get methods added

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 import express from "express";
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Hello world');
+});
+
+app.post('/auth/login', (req, res) => {
+  res.json({
+    success: true,
+  })
+});
+
+app.listen(4444, (err) => {
+  if (err) {
+    return console.log(err);
+  }
+  console.log('Server ok!')
+})
