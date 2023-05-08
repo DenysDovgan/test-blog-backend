712136bc8633270d74ad694aa9be158f87dc4ce4
DenysDovgan <dovgan.denys.ws@gmail.com>
2023-05-08 17:27:10 +0200

    auth and register added but need fix

--- index.js
+++ index.js
@@ -1,31 +1,66 @@
 import express from "express";
 import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt";
+import mongoose from "mongoose";
+import { validationResult } from "express-validator";
+
+import {registerValidation} from './validations/auth.js';
+
+import UserModel from './models/User.js';
+
+mongoose
+  .connect('mongodb+srv://ytcsgoatlas:qazwsxedc123@cluster0.0snbrof.mongodb.net/blog?retryWrites=true&w=majority',)
+  .then(() => console.log('DB OK!'))
+  .catch(() => console.log('DB ERROR', err))
+
 
 const app = express();
 
 app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.send('Hello world');
-});
-
-app.post('/auth/login', (req, res) => {
-  console.log(req.body);
-
-
-
-  if(req.body.email === 'test@test.com') {
-    const token = jwt.sign({
+app.post('/auth/register', registerValidation, async (req, res) => {
+  try {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json(errors.array());
+    }
+  
+    const password = req.body.password;
+    const salt = await bcrypt.genSalt(10);
+    const passwordHash = await bcrypt.hash(password, salt);
+  
+    const doc = new UserModel({
       email: req.body.email,
-      fullName: 'Alex Yager',
-    }, 'secret123');
+      fullName: req.body.fullName,
+      avatarUrl: req.body.avatarUrl,
+      passwordHash,
+    });
+  
+    const user = await doc.save();
+
+    const token = jwt.sign(
+      {
+        _id: user._id,
+      },
+      'secret123',
+      {
+        expiresIn: '30d',
+      },
+      );
+  
+
+
+    res.json({
+      user,
+      token,
+    })
+  
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({
+      message: 'Register error',
+    });
   }
-
-
-  res.json({
-    success: true,
-    token,
-  })
 });
 
 app.listen(4444, (err) => {
