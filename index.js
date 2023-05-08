4e010b7011357002cb6ec507ccd21527d6c59a7e
DenysDovgan <dovgan.denys.ws@gmail.com>
2023-05-08 12:25:36 +0200

    Added json web token

--- index.js
+++ index.js
@@ -1,14 +1,30 @@
 import express from "express";
+import jwt from "jsonwebtoken";
 
 const app = express();
+
+app.use(express.json());
 
 app.get('/', (req, res) => {
   res.send('Hello world');
 });
 
 app.post('/auth/login', (req, res) => {
+  console.log(req.body);
+
+
+
+  if(req.body.email === 'test@test.com') {
+    const token = jwt.sign({
+      email: req.body.email,
+      fullName: 'Alex Yager',
+    }, 'secret123');
+  }
+
+
   res.json({
     success: true,
+    token,
   })
 });
 
