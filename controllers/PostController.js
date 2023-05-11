1ba524a31337985a1c295ccb8ed030f5e54ad405
DenysDovgan <dovgan.denys.ws@gmail.com>
2023-05-11 23:47:57 +0200

    HTTP Requests responses

--- controllers/PostController.js
+++ controllers/PostController.js
@@ -1,15 +1,83 @@
-import PostModel from '../models/Post.js';
+import PostModel from "../models/Post.js";
 
 export const getAll = async (req, res) => {
   try {
-    const posts = await PostModel.find();
+    const posts = await PostModel.find().populate("user").exec();
 
     res.json(posts);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({
+      message: "Post found failed",
+    });
+  }
+};
+
+export const getOne = (req, res) => {
+  try {
+    const postId = req.params.id;
+
+    PostModel.findOneAndUpdate(
+      {
+        _id: postId,
+      },
+      {
+        $inc: { viewsCount: 1 },
+      },
+      {
+        returnDocument: "after",
+      }
+    )
+      .then((doc) => {
+        if (!doc) {
+          return res.status(404).json({
+            message: "Post did not found",
+          });
+        }
+        res.json(doc);
+      })
+      .catch((err) => {
+        console.log(err);
+        res.status(500).json({
+          message: "Post return failed",
+        });
+      });
   } catch (error) {
     console.log(error);
     res.status(500).json({
-      message: 'Post found failed',
+      message: "Cannot get the post",
+    });
+  }
+};
+
+export const remove = (req, res) => {
+  try {
+    const postId = req.params.id;
+
+    PostModel.findOneAndDelete({
+      _id: postId,
     })
+      .then((doc) => {
+        if (!doc) {
+          return res.status(404).json({
+            message: "Post did not found",
+          });
+        }
+        res.json({
+          success: true,
+        });
+      })
+      .catch((err) => {
+        console.log(err);
+        res.status(500).json({
+          message: "Post deletion failed",
+        });
+      });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      message: "Cannot get the post",
+    });
   }
 };
 
@@ -25,11 +93,39 @@ export const create = async (req, res) => {
 
     const post = await doc.save();
 
-    res.json({post});
-  } catch (error) {
-    console.log(error);
+    res.json({ post });
+  } catch (err) {
+    console.log(err);
     res.status(500).json({
-      message: 'Post crating failed',
-    })
+      message: "Post crating failed",
+    });
   }
-}
+};
+
+export const update = async (req, res) => {
+  try {
+    const postId = req.params.id;
+
+    await PostModel.updateOne(
+      {
+        _id: postId,
+      },
+      {
+        title: req.body.title,
+        text: req.body.text,
+        imageUrl: req.body.imageUrl,
+        user: req.userId,
+        tags: req.body.tags,
+      }
+    );
+
+    res.json({
+      success: true,
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({
+      message: "Post update failed",
+    });
+  }
+};
