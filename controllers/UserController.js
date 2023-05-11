dfea829338a8196a6f00bb2cda222e91afa2088e
DenysDovgan <dovgan.denys.ws@gmail.com>
2023-05-12 00:30:41 +0200

    Refactory

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -1,50 +1,42 @@
 import jwt from "jsonwebtoken";
 import bcrypt from "bcrypt";
-import { validationResult } from "express-validator";
-
-import UserModel from '../models/User.js';
+import UserModel from "../models/User.js";
 
 export const register = async (req, res) => {
   try {
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-      return res.status(400).json(errors.array());
-    }
-  
     const password = req.body.password;
     const salt = await bcrypt.genSalt(10);
     const hash = await bcrypt.hash(password, salt);
-  
+
     const doc = new UserModel({
       email: req.body.email,
       fullName: req.body.fullName,
       avatarUrl: req.body.avatarUrl,
       passwordHash: hash,
     });
-  
+
     const user = await doc.save();
 
     const token = jwt.sign(
       {
         _id: user._id,
       },
-      'secret123',
+      "secret123",
       {
-        expiresIn: '30d',
-      },
-      );
-  
-    const {passwordHash, ...userData} = user._doc;
+        expiresIn: "30d",
+      }
+    );
+
+    const { passwordHash, ...userData } = user._doc;
 
     res.json({
       ...userData,
       token,
     });
-  
   } catch (err) {
     console.log(err);
     res.status(500).json({
-      message: 'Register error',
+      message: "Register error",
     });
   }
 };
@@ -55,15 +47,18 @@ export const login = async (req, res) => {
 
     if (!user) {
       return res.status(404).json({
-        message: 'User is not found',
-      })
+        message: "User is not found",
+      });
     }
 
-    const isValidPass = await bcrypt.compare(req.body.password, user._doc.passwordHash);
+    const isValidPass = await bcrypt.compare(
+      req.body.password,
+      user._doc.passwordHash
+    );
 
     if (!isValidPass) {
       return res.status(404).json({
-        message: 'Login or password is incorrect',
+        message: "Login or password is incorrect",
       });
     }
 
@@ -71,42 +66,41 @@ export const login = async (req, res) => {
       {
         _id: user._id,
       },
-      'secret123',
+      "secret123",
       {
-        expiresIn: '30d',
-      },
+        expiresIn: "30d",
+      }
     );
-    const {passwordHash, ...userData} = user._doc;
+    const { passwordHash, ...userData } = user._doc;
 
     res.json({
       ...userData,
       token,
     });
-
   } catch (err) {
     console.log(err);
-      res.status(400).json({
-      message: 'Login error',
+    res.status(400).json({
+      message: "Login error",
     });
   }
 };
 
 export const getMe = async (req, res) => {
   try {
-    const user = await UserModel.findById(req.userId); 
+    const user = await UserModel.findById(req.userId);
 
     if (!user) {
       return res.status(404).json({
-        message: 'Did not found',
-      })
+        message: "Did not found",
+      });
     }
 
-    const {passwordHash, ...userData} = user._doc;
+    const { passwordHash, ...userData } = user._doc;
 
     res.json(userData);
   } catch (error) {
     res.status(403).json({
-      message: 'No access',
+      message: "No access",
     });
   }
 };
